ebc812ab5abdfb2213a7144df1616d2e535fde41
Neil Martin <neilm@4js-emea.com>
2019-03-01 00:17:42 +0000

    env.sh and readme

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ Goal is to be able to build and run using:
 * Linux command line with a makefile
 * Genero Studio
 
-Also be able to build and deploy as a GAR file.
+Also to be able to build and deploy as a GAR file and the custom GBC zip.
+
+NOTE: The env.sh script checks the environment and reports the versions found and warns for any missing prerequisites.
+
+Instructions:
+* download or clone the repo
+* set your Genero environment
+* cd simpleWCGBCdemo
+* . env.sh
+* make
+
+eg:
+```
+```
+
---
+++ env.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+GVER=$(fglrun -V 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f1-2)
+case $GVER in
+3.10)
+  GENVER=310
+  ;;
+3.20)
+  GENVER=320
+  ;;
+*)
+	echo "ERROR: Genero not found or invalid version $GVER !"
+esac
+
+export GENVER
+
+JAVAVER=$(javac -version 2> /dev/null 2>&1)
+if [ $? -ne 0 ]; then
+	echo "ERROR: Java compiler not found!"
+fi
+
+NODEJS=$(nodejs -v 2> /dev/null)
+if [ $? -ne 0 ]; then
+	echo "ERROR: NODEJS not found!"
+fi
+
+GRUNT=$(grunt -V 2> /dev/null)
+if [ $? -ne 0 ]; then
+	echo "ERROR: Grunt not found!"
+fi
+
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-8-openjdk-amd64}
+JVM=$(find $JAVA_HOME -name libjvm.so)
+if [ $? -ne 0 ]; then
+	echo "ERROR: Java VM not found!"
+else
+	export LD_LIBRARY_PATH=$(dirname $JVM)
+fi
+
+if [ ! -d gbc/gbc-current ]; then
+	echo "Enter full path to current gbc project extract folder ?"
+	read GBCDIR
+	if [ ! -d $GBCDIR ] || [ ! -e $GBCDIR/VERSION ]; then
+		echo "ERROR: GBC Project folder not found!"
+	else
+		ln -s $GBCDIR gbc/gbc-current
+	fi	
+fi
+
+echo Genero is $GVER
+echo Java $JAVAVER JVM is $JVM
+echo GBC $(cat gbc/gbc-current/VERSION)
+echo NODEJS $NODEJS + $GRUNT
